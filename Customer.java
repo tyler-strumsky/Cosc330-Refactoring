c9dd39037deb508761b88e50c5c29b72488d09be
tyler-strumsky <78280665+tyler-strumsky@users.noreply.github.com>
2022-12-14 20:17:27 -0500

    Update Frequent Rentals

--- Customer.java
+++ Customer.java
@@ -18,29 +18,37 @@ public class Customer {
         rentals.add(rental);
     }
 
+    public double getTotalCharge()
+    {
+        double total = 0;
+        for (Rental rental : rentals) {
+           
+            total += rental.getCharge();
+        }
+        return total;
+    }
 
+    public double getFrequentRenterPoints()
+    {
+        double total = 0;
+        for (Rental rental : rentals) {
+           
+            total += rental.getCharge();
+        }
+        return total;
+    }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-
+        
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
            
-            double amount = rental.getCharge();
-
-            // add frequent renter points
-            frequentRenterPoints+= rental.getFrequentRentalPoints();
-
-
             // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
-
-            totalAmount += amount;
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
         }
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getFrequentRenterPoints()) + " frequent renter points";
 
         return result;
     }
