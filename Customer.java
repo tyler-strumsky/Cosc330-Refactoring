a24b6efd0765c26a51a2ee99cb826b7d31e32ef2
tyler-strumsky <78280665+tyler-strumsky@users.noreply.github.com>
2022-12-14 19:35:03 -0500

    Step Three Commit

--- Customer.java
+++ Customer.java
@@ -18,27 +18,9 @@ public class Customer {
         rentals.add(rental);
     }
 
-    public double amountFor(Rental rental)
+    public double amountFor(Rental currentRental) 
     {
-        double amount = 0;
-
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (rental.getDaysRented() > 2)
-                    amount += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                amount += 1.5;
-                if (rental.getDaysRented() > 3)
-                    amount += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-
-        return amount;
+        return currentRental.getCharge();
     }
 
 
