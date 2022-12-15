74c4cc5aa6f2fcb3fcb7e61515d8fa160f3186bf
tyler-strumsky <78280665+tyler-strumsky@users.noreply.github.com>
2022-12-14 19:43:00 -0500

    Step-Three commit again

--- Rental.java
+++ Rental.java
@@ -21,25 +21,25 @@ public class Rental {
 
     public double getCharge()
     {
-        double amount = 0;
+        double total = 0;
 
         switch (getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                amount += 2;
+                total += 2;
                 if (getDaysRented() > 2)
-                    amount += (getDaysRented() - 2) * 1.5;
+                    total += (getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                amount += getDaysRented() * 3;
+                total += getDaysRented() * 3;
                 break;
             case Movie.CHILDREN:
-                amount += 1.5;
+            total += 1.5;
                 if (getDaysRented() > 3)
-                    amount += (getDaysRented() - 3) * 1.5;
+                    total += (getDaysRented() - 3) * 1.5;
                 break;
         }
 
-        return amount;
+        return total;
     }
 
 }
