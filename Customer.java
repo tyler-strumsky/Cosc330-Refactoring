3227b702f88d291274a651809bd01d7f0ef20f26
tyler-strumsky <78280665+tyler-strumsky@users.noreply.github.com>
2022-12-14 20:11:47 -0500

    getTotalCharge

--- Customer.java
+++ Customer.java
@@ -18,6 +18,16 @@ public class Customer {
         rentals.add(rental);
     }
 
+    public double getTotalCharge()
+    {
+        double total = 0;
+        for (Rental rental : rentals) {
+           
+            total += rental.getCharge();
+        }
+        return total;
+    }
+
 
 
     public String statement() {
@@ -27,19 +37,16 @@ public class Customer {
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
            
-            double amount = rental.getCharge();
-
             // add frequent renter points
             frequentRenterPoints+= rental.getFrequentRentalPoints();
 
 
             // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
 
-            totalAmount += amount;
         }
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
         return result;
